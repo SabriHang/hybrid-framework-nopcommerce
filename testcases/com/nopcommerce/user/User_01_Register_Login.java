7c82a82eef49f68fab4f1c5248bbbd6bc7259936
SabriHang <hoanghang29691@gmail.com>
2023-05-17 14:50:47 +0700

    Init automatio framework

---
+++ testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -0,0 +1,28 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class User_01_Register_Login {
+	@BeforeTest
+	public void beforeTest() {
+	}
+	
+	@Test
+	public void Test_01() {
+	}
+	
+	@Test
+	public void Test_02() {
+	}
+	
+	@Test
+	public void Test_03() {
+	}
+
+	@AfterTest
+	public void afterTest() {
+	}
+
+}
